e703abc1f8262104e9489e74ca7f4522c7f8c2da
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-12 12:22:50 -0300

    Feminicidio formulário usando firebase

---
+++ FormsFeminicidio/firebase-config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "95785ee6de6256fd251cafa9d5df051903027c80",
+    authDomain: "blog-9170b.firebaseapp.com",
+    projectId: "438477786016",
+    storageBucket: "blog-9170b.appspot.com",
+    messagingSenderId: "438477786016",
+    appId: "blog-9170b"
+   };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
